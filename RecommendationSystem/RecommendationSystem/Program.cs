14008835fa283d63b6fff207aa1a56f4f86ff4cc
swenmeeuwes <swenmeeu@gmail.com>
2017-05-01 16:14:02 +0200

    Created initial project

---
+++ RecommendationSystem/RecommendationSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommendationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
