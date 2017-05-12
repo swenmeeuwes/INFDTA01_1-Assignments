51782526f84e82e59ca7d208ff8daa8b0090cd2c
swenmeeuwes <swenmeeu@gmail.com>
2017-05-12 11:44:30 +0200

    Implemented prediction of ratings

---
+++ RecommendationSystem/RecommendationSystem/useritem/UserExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using RecommendationSystem.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommendationSystem.useritem
+{
+    static class UserExtensionMethods
+    {
+        public static ArticleRating[] PredictRatings(this User[] users)
+        {
+            var predictions = new List<ArticleRating>();
+
+            var availableRatings = users.SelectMany(u => u.articleRatings).Select(r => r.ArticleNumber).Distinct().ToArray();
+            for (int i = 0; i < availableRatings.Length; i++)
+            {
+                var numeratorSummation = 0d;
+                for (int j = 0; j < users.Length; j++)
+                {
+                    var userRating = users[j].articleRatings.SingleOrDefault(r => r.ArticleNumber == availableRatings[i]);
+                    if (userRating == null)
+                        continue;
+
+                    numeratorSummation += users[j].Similarity * userRating.Rating;
+                }
+                var predictionRating = numeratorSummation / users.Sum(u => u.Similarity);
+                predictions.Add(new ArticleRating()
+                {
+                    ArticleNumber = availableRatings[i],
+                    Rating = predictionRating
+                });
+            }
+
+            return predictions.ToArray();
+        }
+    }
+}
