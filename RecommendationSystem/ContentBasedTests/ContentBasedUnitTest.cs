b452b10a925b3b554a819f756dda175c72ae2b8a
Swen Meeuwes <swenmeeu@gmail.com>
2017-06-18 13:11:50 +0200

    Updated unit tests

--- RecommendationSystem/ContentBasedTests/ContentBasedUnitTest.cs
+++ RecommendationSystem/ContentBasedTests/ContentBasedUnitTest.cs
@@ -13,6 +13,7 @@ namespace ContentBasedTests
         private readonly static string FILE_PATH = "assets/userItem.data";
         private readonly static char DELIMITER = ',';
 
+        private ContentBasedDataWrapper dataWrapper;
         private Dictionary<string, User> data;
         private DeviationMatrix deviationMatrix;
 
@@ -21,15 +22,57 @@ namespace ContentBasedTests
         {
             data = DataProvider.GetData(FILE_PATH, DELIMITER);
             deviationMatrix = DeviationMatrixFactory.Instance.Create(data);
+
+            dataWrapper = new ContentBasedDataWrapper(data, deviationMatrix);
         }
 
         [TestMethod]
-        public void TestContentBased()
+        public void TestPredictionAlgorithm()
         {
-            var user7 = data["7"];
-            var prediction = deviationMatrix.Predict(user7, "101");
-            var doublesAreEqual = Math.Abs(2.60714285714286d - prediction.Rating) / prediction.Rating <= 0.0000001d;
-            Assert.IsTrue(doublesAreEqual);
+            // UserID, targetItem, expectedValue
+            Tuple<string, string, double>[] testCases = new Tuple<string, string, double>[] {
+                new Tuple<string, string, double>("7", "101", 2.60714285714286),
+                new Tuple<string, string, double>("7", "103", 2.16666666666667),
+                new Tuple<string, string, double>("7", "106", 3.17647058823529),
+
+                new Tuple<string, string, double>("3", "103", 2.16666666666667),
+                new Tuple<string, string, double>("3", "105", 2.33333333333333)
+            };
+
+            // Execute each test case
+            foreach (var testCase in testCases)
+            {
+                var user = data[testCase.Item1];
+                var prediction = deviationMatrix.Predict(user, testCase.Item2);
+                var passed = Math.Abs(testCase.Item3 - prediction.Rating) / prediction.Rating <= 0.0000001d;
+                Assert.IsTrue(passed);
+            }
+        }
+
+        [TestMethod]
+        public void TestPredictionAlgorithmAfterChange()
+        {
+            // Add the new rating to the system
+            var userThatRated = data["3"];
+            dataWrapper.AddRatings(userThatRated, new ArticleRating[] {
+                new ArticleRating() { ArticleNumber = "105", Rating = 4.0d }
+            });
+
+            // UserID, targetItem, expectedValue
+            Tuple<string, string, double>[] testCases = new Tuple<string, string, double>[] {
+                new Tuple<string, string, double>("7", "101", 2.4),
+                new Tuple<string, string, double>("7", "103", 2.16666666666667),
+                new Tuple<string, string, double>("7", "106", 3.05555555555556)               
+            };
+
+            // Execute each test case
+            foreach (var testCase in testCases)
+            {
+                var user = data[testCase.Item1];
+                var prediction = deviationMatrix.Predict(user, testCase.Item2);
+                var passed = Math.Abs(testCase.Item3 - prediction.Rating) / prediction.Rating <= 0.0000001d;
+                Assert.IsTrue(passed);
+            }
         }
     }
 }
