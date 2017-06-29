1d24aadc82e5a6ac017610597542acd79455bee6
Swen Meeuwes <swenmeeu@gmail.com>
2017-06-29 17:10:23 +0200

    Optimized prediction computation + added an overload method with the minimum amount of neighbours that should have rated the article

--- RecommendationSystem/Collaborative User-Item/useritem/UserExtensionMethods.cs
+++ RecommendationSystem/Collaborative User-Item/useritem/UserExtensionMethods.cs
@@ -16,11 +16,37 @@ namespace UserItem.useritem
             var availableRatings = users.SelectMany(u => u.articleRatings).Select(r => r.ArticleNumber).Distinct().ToArray();
             for (int i = 0; i < availableRatings.Length; i++)
             {
-                // Can be optimized by implementing it single-pass
-                var numeratorSummation = users.Where(u => u.articleRatings.Any(r => r.ArticleNumber == availableRatings[i]))
-                                                .Sum(u => u.Similarity * u.articleRatings.First(r => r.ArticleNumber == availableRatings[i]).Rating);
-                var denominatorSummation = users.Where(u => u.articleRatings.Any(r => r.ArticleNumber == availableRatings[i]))
-                                                .Sum(u => u.Similarity);
+                var neighbours = users.Where(u => u.articleRatings.Any(r => r.ArticleNumber == availableRatings[i])); // Neighbours that rated the article
+                var numeratorSummation = neighbours.Sum(u => u.Similarity * u.articleRatings.First(r => r.ArticleNumber == availableRatings[i]).Rating);
+                var denominatorSummation = neighbours.Sum(u => u.Similarity);
+
+                var predictionRating = numeratorSummation / denominatorSummation;
+                predictions.Add(new ArticleRating()
+                {
+                    ArticleNumber = availableRatings[i],
+                    Rating = predictionRating
+                });
+            }
+
+            return predictions.ToArray();
+        }
+
+        public static ArticleRating[] PredictRatings(this User[] users, int minNeighbours)
+        {
+            var predictions = new List<ArticleRating>();
+
+            var availableRatings = users.SelectMany(u => u.articleRatings).Select(r => r.ArticleNumber).Distinct().ToArray();
+            for (int i = 0; i < availableRatings.Length; i++)
+            {
+                var neighbours = users.Where(u => u.articleRatings.Any(r => r.ArticleNumber == availableRatings[i])); // Neighbours that rated the article
+
+                // If not enough neighbours rated the article, skip the prediction for this article
+                if (neighbours.Count() < minNeighbours)
+                    continue;
+
+                var numeratorSummation = neighbours.Sum(u => u.Similarity * u.articleRatings.First(r => r.ArticleNumber == availableRatings[i]).Rating);
+                var denominatorSummation = neighbours.Sum(u => u.Similarity);
+
                 var predictionRating = numeratorSummation / denominatorSummation;
                 predictions.Add(new ArticleRating()
                 {
